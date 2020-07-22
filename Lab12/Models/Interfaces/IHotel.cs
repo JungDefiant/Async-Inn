57a6490ed682aa5c079e90ff54874e979d893166
JungDefiant <jungdefiant@gmail.com>
2020-07-22 14:01:31 -0700

    Updated HotelRepository and IHotel

---
+++ Lab12/Models/Interfaces/IHotel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12.Models.Interfaces
+{
+    interface IHotel
+    {
+        Task<Hotel> Create(Hotel hotel);
+
+        Task<List<Hotel>> GetHotels();
+
+        Task<Hotel> GetHotel(int id);
+
+        Task<Hotel> Update(int id, Hotel hotel);
+
+        Task Delete(int id);
+    }
+}
---
+++ Lab12/Models/Services/HotelRepository.cs
@@ -0,0 +1,56 @@
+﻿using Lab12.Data;
+using Lab12.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab12.Models.Services
+{
+    public class HotelRepository : IHotel
+    {
+        private AsyncInnDbContext _context;
+        
+        public HotelRepository(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Hotel> Create(Hotel hotel)
+        {
+            _context.Entry(hotel).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            await _context.SaveChangesAsync();
+            return hotel;
+        }
+
+        public async Task Delete(int id)
+        {
+            Hotel hotel = await GetHotel(id);
+
+            _context.Entry(hotel).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Hotel> GetHotel(int id)
+        {
+            Hotel hotel = await _context.Hotels.FindAsync(id);
+            return hotel;
+        }
+
+        public async Task<List<Hotel>> GetHotels()
+        {
+            var hotels = await _context.Hotels.ToListAsync();
+            return hotels;
+        }
+
+        public async Task<Hotel> Update(int id, Hotel hotel)
+        {
+            _context.Entry(hotel).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return hotel;
+        }
+    }
+}
